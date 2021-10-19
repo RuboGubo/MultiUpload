aeedc46b86568f10b1b81843e7cb09e4e0210e25
RuboGubo <ruben.john.ward@gmail.com>
2021-10-19 21:40:35 +0100

    work + prospectus

--- SignIn/SignIn.py
+++ SignIn/SignIn.py
@@ -1,38 +1,42 @@
 import json
 
-f = open(r'SignIn\UserPreferances.json', 'r')
-data = json.load(f)
-f.close()
+from Youtube.MonitorVideos import YoutubeChannel
 
-print('Please choose a platform')
-print('Suported Platforms:')
+with open(r'SignIn/UserPreferances.json', 'r') as f:
+    UserPrefferances = json.load(f)
+    f.close()
+
+
+def ConfigureYoutube():
+    UserChannel = YoutubeChannel(True)
+    UserPrefferances['PlatfromSpecificDetails']['youtube']['ChannelLogoDir'] = UserChannel.getChannelLogo()
+    UserPrefferances['PlatfromSpecificDetails']['youtube']['ChannelName'] = UserChannel.CHANNELTITLE
+    UserPrefferances['PlatfromSpecificDetails']['youtube']['SetUp'] = True
+    
+
+def ConfigureDailyMotion():
+    UserPrefferances['PlatfromSpecificDetails']['dailymotion']['SetUp'] = True
+    
+print('Please choose unconfigured platform')
 print('YouTube(YT)')
 print('PeerTube(PT)')
 print('DailyMotion(DM)')
 
 Service = input(':')
 
-if Service.upper() == 'YT': # make it add the platforms to the platforms area of the prefersances json
-    print('YouTube Setup')
-    print()
-    print('Note: You must be verified. Go to https://www.youtube.com/verify')
-    
-    from Youtube.Google import Create_Service
-
-    CLIENT_SECRET_FILE = 'client-secret.json'
-    API_NAME = 'youtube'
-    API_VERSION = 'v3'
-    SCOPES = ['https://www.googleapis.com/auth/youtube', 'https://www.googleapis.com/auth/youtube.upload']
-
-    service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
-    
-    if 'Youtube' not in data['Platforms']:
-        data['Platforms'].append('Youtube')
+if Service.upper() == 'YT' and not UserPrefferances['PlatfromSpecificDetails']['youtube']['SetUp']:
+    ConfigureYoutube()
     
 elif Service.upper() == 'PT':
     print('PeerTube Setup Disabled')
-elif Service.upper() == 'DM':
-    print('DailyMotion Setup Disabled')
     
-    if 'DailyMotion' not in data['Platforms']:
-        data['Platforms'].append('DailyMotion')
+elif Service.upper() == 'DM' and not UserPrefferances['PlatfromSpecificDetails']['dailymotion']['SetUp']:
+    ConfigureDailyMotion()
+
+else:
+    print('Service already exists or wrong code used')
+        
+        
+with open(r'SignIn/UserPreferances.json', 'w') as f:
+    json.dump(UserPrefferances, f)
+    f.close()
